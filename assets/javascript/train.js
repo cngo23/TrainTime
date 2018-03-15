8cedf93664d31611e7288399c4f98a8cb2cc98a0
Christian Ngo <ngochristian23@gmail.com>
2018-03-15 01:26:38 -0500

    adds for loop to append but errors occur

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,57 +1,119 @@
-//Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBgV1W6PnPs8OD2S3rxHThniB_NCdLNiZU",
-    authDomain: "traintime-24541.firebaseapp.com",
-    databaseURL: "https://traintime-24541.firebaseio.com",
-    projectId: "traintime-24541",
-    storageBucket: "traintime-24541.appspot.com",
-    messagingSenderId: "523003573186"
-  };
-  firebase.initializeApp(config);
+$(document).ready(function () {
 
-  var database = firebase.database();
+    //Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBgV1W6PnPs8OD2S3rxHThniB_NCdLNiZU",
+        authDomain: "traintime-24541.firebaseapp.com",
+        databaseURL: "https://traintime-24541.firebaseio.com",
+        projectId: "traintime-24541",
+        storageBucket: "traintime-24541.appspot.com",
+        messagingSenderId: "523003573186"
+    };
+    firebase.initializeApp(config);
 
-  var train = "";
-  var destination = "";
-  var firstDepartTime = 0;
-  var frequency = 0;
+    var database = firebase.database();
+    var ref = database.ref("trains");
 
-  $("#addTrain").on("click", function() {
-      event.preventDefault();
-      
+    var train = "";
+    var destination = "";
+    var firstDepartTime = 0;
+    var frequency = 0;
 
-      train = $("#trainInput").val().trim();
-      destination = $("#destInput").val().trim();
-      firstDepartTime = $("#firstDepartTime").val().trim();
-      frequency = $("#freqInput").val().trim();
+    ref.on("value", getData, errData);
+    $("#addTrain").on("click", function () {
+        event.preventDefault();
 
-      console.log(train);
-      console.log(destination);
-      console.log(firstDepartTime);
-      console.log(frequency);
+        train = $("#trainInput").val().trim();
+        destination = $("#destInput").val().trim();
+        firstDepartTime = $("#firstDepartTime").val().trim();
+        frequency = $("#freqInput").val().trim();
 
-      database.ref().push({
-          train: train,
-          destination: destination,
-          firstDepartTime: firstDepartTime,
-          frequency: frequency
-      });
+        console.log(train);
+        console.log(destination);
+        console.log(firstDepartTime);
+        console.log(frequency);
 
-      
-  });
-  $(".adds").remove();
+        //time conversion
+        var firstTimeConverted = moment(firstDepartTime, "hh:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
 
-  database.ref().on("value", function(snapshot) {
-      snapshot.forEach(function(childSnapshot){
-          $("#trainInfo").append(`<tr class="adds"> 
-          <td> ${train} </td> 
-          <td> ${destination} </td>
-          <td> ${frequency} </td>
-          <td> ${firstDepartTime} </td> 
-          <td> will know later </td>
-           </tr>`);
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-      })
-  })
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("difference in time: " + diffTime);
 
-  
+        var tRemainder = diffTime % frequency;
+        console.log(tRemainder);
+
+        var minTillTrain = frequency - tRemainder;
+        console.log("minutes till train: " + minTillTrain);
+
+        var nextTrain = moment().add(minTillTrain, "minutes");
+        var nextTrainFormat = moment(nextTrain).format("hh:mm");
+        console.log(nextTrain);
+        console.log(nextTrainFormat);
+
+        var data = {
+            train: train,
+            destination: destination,
+            firstDepartTime: firstDepartTime,
+            frequency: frequency,
+            nextTrainFormat: nextTrainFormat,
+            minTillTrain: minTillTrain
+        }
+
+        ref.push(data);
+        ref.once("value", getData, errData);
+        $(".text").val("");
+
+    });
+
+    function getData(data) {
+
+        // ref.on("child_added", function (snapshot) {
+
+        //     console.log(snapshot.val());
+
+        //     console.log(snapshot.val().train);
+
+        //     console.log(snapshot.val().destination);
+
+        //     console.log(snapshot.val().firstDepartTime);
+
+        //     console.log(snapshot.val().frequency);
+
+        //     console.log(snapshot.val().nextTrainFormat);
+
+        //     console.log(snapshot.val().minTillTrain);
+
+        // })
+
+
+          var trains = data.val();
+          var keys = Object.keys(data);
+
+          $("#tableInfo").empty();
+          for(var i = 0; i < keys.length; i++) {
+              var tRow = $("<tr>");
+              var k = keys[i];
+
+          $("<td scope='col'>").text(trains[k].train).appendTo(tRow);
+          $("<td scope='col'>").text(trains[k].destination).appendTo(tRow);
+          $("<td scope='col'>").text(trains[k].frequency).appendTo(tRow);
+
+          $("<td scope='col'>").text(trains[k].nextTrainFormat).appendTo(tRow);
+          $("<td scope='col'>").text(trains[k].minTillTrain).appendTo(tRow);
+
+          $("#tableInfo").append(tRow);
+
+        }
+    }
+
+    function errData(err) {
+        console.log("Oh NO! Error!");
+        console.log(err);
+    }
+
+});
+
